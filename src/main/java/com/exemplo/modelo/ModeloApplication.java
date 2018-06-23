bc610f87751060ccfba731c06df02b06ef000539
CRISTIANO BOELL <cristiano.boell@bomjesus.br>
2018-06-23 11:34:01 -0300

    FIX pom.xml to openshift

--- src/main/java/com/exemplo/modelo/ModeloApplication.java
+++ src/main/java/com/exemplo/modelo/ModeloApplication.java
@@ -1,12 +1,25 @@
 package com.exemplo.modelo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class ModeloApplication {
+public class ModeloApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ModeloApplication.class);
+    }	
 }
